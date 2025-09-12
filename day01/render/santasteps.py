f0468f88b3eb5b1261000c65ff7ec646612ef8a8
WedgeBeard <tysonstecklein@gmail.com>
2025-09-12 16:51:04 -0600

    now drawing first set of steps on screen

--- day01/render/santasteps.py
+++ day01/render/santasteps.py
@@ -5,40 +5,48 @@ import pprint
 # constants
 step_file = join("day01", "render", "step.png")
 step_fill_file = join("day01", "render", "step_fill.png")
+WINDOW_WIDTH, WINDOW_HEIGHT = 256, 240
+SPRITE_WIDTH, SPRITE_HEIGHT = 16, 16
+SPEED = 0.009
 
 # general setup
 pygame.init()
-WINDOW_WIDTH, WINDOW_HEIGHT = 256, 240
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SCALED)
 pygame.display.set_caption("Santa vs. Stairs")
 running = True
 
-# surface
+# surfaces
 step = pygame.image.load(step_file).convert_alpha()
 step_fill = pygame.image.load(step_fill_file).convert()
 
-steps_on_screen = []
-start_pos = (0, (WINDOW_HEIGHT/2)-16)
+# calculate all step heights
+step_positions = []
+start_pos = (int(0), int((WINDOW_HEIGHT/2)-SPRITE_HEIGHT))
 curr_pos = start_pos
-steps_on_screen.append(curr_pos)
+step_positions.append(curr_pos)
 
 filename = "day01/input.txt"
 with open(filename) as opened_file:
     line = opened_file.readline()
 
-print(len(line))
 floor = 0
 for i, char in enumerate(line, 1):
     if char == "(":
         floor += 1
-        steps_on_screen.append((steps_on_screen[i-1][0]+16, steps_on_screen[i-1][1]+16))
+        step_positions.append((step_positions[i-1][0]+SPRITE_WIDTH, step_positions[i-1][1]+SPRITE_HEIGHT))
     elif char == ")":
         floor -= 1
-        steps_on_screen.append((steps_on_screen[i-1][0]+16, steps_on_screen[i-1][1]-16))
-    
+        step_positions.append((step_positions[i-1][0]+SPRITE_WIDTH, step_positions[i-1][1]-SPRITE_HEIGHT))
 
+max_sprites_on_screen = int(WINDOW_WIDTH/SPRITE_WIDTH) + 1
+curr_step = 0
+
+# # iterate through 17 steps starting at curr_step
+# for i, cstep in enumerate(step_positions[curr_step:curr_step+max_sprites_on_screen], start=curr_step):
+#     print(cstep)
+
+# event loop
 while running:
-    # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -46,13 +54,14 @@ while running:
     # draw the game background
     display_surface.fill(pygame.Color(49, 54, 110))
 
-    # first step
-    curr_pos = (0, (WINDOW_HEIGHT/2)-16)
-    display_surface.blit(step, (curr_pos[0], curr_pos[1]))
-    while curr_pos[1] < WINDOW_HEIGHT:
-        curr_pos = (curr_pos[0], curr_pos[1]+16)
-        display_surface.blit(step_fill, (curr_pos[0], curr_pos[1]))
-
+    # draw steps in view
+    for i, step_pos in enumerate(step_positions[curr_step:curr_step+max_sprites_on_screen], start=curr_step):
+        display_surface.blit(step, (step_pos[0], step_pos[1]))
+        next_pos = step_pos
+        while next_pos[1] < WINDOW_HEIGHT:
+            next_pos = (next_pos[0], next_pos[1]+SPRITE_HEIGHT)
+            display_surface.blit(step_fill, (next_pos[0], next_pos[1]))
+        step_pos = (step_pos[0]-SPEED, step_pos[1])
     pygame.display.flip()
 
 pygame.quit()
